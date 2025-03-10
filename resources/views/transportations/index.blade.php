ed9aee2f77f9e123e5d84a27ffa2586f1f713810
AdiApriyanto <adiapriyanto935@gmail.com>
2025-03-10 16:31:20 +0700

    Layout Index Transportation

--- resources/views/transportations/index.blade.php
+++ resources/views/transportations/index.blade.php
@@ -5,34 +5,101 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-5xl sm:px-6 lg:px-6">
-            <div class="mx-36">
-                <div class="font-extrabold text-[26px] text-[#0D0C41]">Form Input Transportasi</div>
-                <div class="text-[#9D9DBC] text-[16px]">Rent and Travel</div>
-                <div class="flex">
-                    <div class="bg-white px-12 py-6 pb-11 mt-4 shadow-xl rounded-[35px] w-full">
-                        <form action="{{route('transportations.store')}}" method="post">
-                            @csrf
-                            <div>
-                                <div class="font-extrabold text-lg mb-2">Kode Transportasi</div>
-                                <input type="text" class="border border-[#D8D8E4] w-full rounded-full h-[55px]"
-                                    name="codeTransportation px-12" placeholder="Kode Transportasi ...">
-                            </div>
-                            <div class="mt-4">
-                                <div class="font-extrabold text-lg mb-2">Jenis Transportasi</div>
-                                <input type="text" class="border border-[#D8D8E4] w-full rounded-full h-[55px]"
-                                    name="codeTransportation px-12" placeholder="Jenis Transportasi ...">
-                            </div>
-                            <div class="mt-8">
-                                <button class="flex items-center justify-center p-4 bg-[#4743FB] font-extrabold text-lg text-white w-full rounded-full h-[55px]">Submit</button>
-                            </div>
-                        </form>
+   <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="gap-5 items-start flex">
+                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg w-1/2 p-4">
+                    <div class="p-4 bg-gray-100 mb-2 rounded-xl font-bold">
+                        FORM INPUT TRANSPORTASI
                     </div>
                     <div>
-                        {{-- data --}}
+                        <form class="max-w-sm mx-auto" id="majorsFormAdd" method="POST">
+                            @csrf
+                            <div class="mb-5">
+                                <label for="codeTransportation"
+                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Kode
+                                    Transportasi</label>
+                                <input type="text" name="codeTransportation"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    required placeholder="Kode Transportasi" value=""  />
+                            </div>
+                            <div class="mb-5">
+                                <label for="transportation"
+                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Transportasi</label>
+                                <input type="text" name="transportation"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    required placeholder="Transportasi" />
+                            </div>
+                            <button type="submit"
+                                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                        </form>
                     </div>
                 </div>
+
+                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg w-full p-4">
+                    <div class="p-4 bg-gray-100 mb-2 rounded-xl font-bold">
+                        TRANSPORTASI
+                    </div>
+                    <div>
+                        <div class="relative overflow-x-auto mt-8">
+                            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                <thead
+                                    class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                    <tr>
+                                        <th scope="col" class="px-6 py-3 bg-gray-100">
+                                            NO
+                                        </th>
+                                        <th scope="col" class="px-6 py-3">
+                                            KODE TRANSPORTASI
+                                        </th>
+                                        <th scope="col" class="px-6 py-3 bg-gray-100">
+                                            TRANSPORTASI
+                                        </th>
+                                        <th scope="col" class="px-6 py-3">
+                                            ACTION
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @php
+                                        $no = 1;
+                                    @endphp
+                                    @foreach ($transportations as $f)
+                                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                            <th scope="row"
+                                                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white bg-gray-100">
+                                                {{ $no++ }}
+                                            </th>
+                                            <td class="px-6 py-4">
+                                                {{ $f->codeTransportation }}
+                                            </td>
+                                            <td class="px-6 py-4 bg-gray-100">
+                                                {{ $f->transportation }}
+                                            </td>
+                                            <td class="px-6 py-4">
+                                                <button type="button"
+                                                    class="bg-amber-400 p-3 w-10 h-10 rounded-xl text-white hover:bg-amber-500"
+                                                    onclick="editSourceModal(this)" data-modal-target="sourceModal"
+                                                    data-id="{{ $f->id }}" data-transportation="{{ $f->transportation }}">
+                                                    <i class="fi fi-sr-file-edit"></i>
+                                                </button>
+                                                <button
+                                                    class="bg-red-400 p-3 w-10 h-10 rounded-xl text-white hover:bg-red-500"
+                                                    onclick="return dataDelete('{{ $f->id }}','{{ $f->transportation }}')">
+                                                    <i class="fi fi-sr-delete-document"></i>
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                        <div class="mt-4">
+                            {{-- {{ $entries->entries() }} --}}
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
